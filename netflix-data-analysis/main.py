3bb6c89621547fcdef2ae4744814577479416359
dklitsch <dklitsch@qualtrax.com>
2021-04-21 11:25:54 -0700

    Break out movies, TV shows, fix director bug.

--- netflix-data-analysis/main.py
+++ netflix-data-analysis/main.py
@@ -1,8 +1,10 @@
 import flask
+from flask import request
 import pandas as pd
 import numpy as np
 from pandas import DataFrame, Series
 from flask_cors import CORS, cross_origin
+import sys
 
 # Create the application.
 app = flask.Flask(__name__)
@@ -15,16 +17,38 @@ def flatten_list(list):
 
 df = pd.read_csv('../data/netflix_titles.csv')
 type_counts = df['type'].value_counts()
-director_counts = df['director'].value_counts()
+director_counts = Series(flatten_list([x.split(', ') for x in df.director.dropna()])).value_counts()
 ccast = Series(flatten_list([x.split(', ') for x in df.cast.dropna()]))
 ccast_counts = ccast.value_counts()
 countries_counts = Series(flatten_list([x.split(', ') for x in df.country.dropna()])).value_counts()
+listed_in_counts = Series(flatten_list([x.split(', ') for x in df.listed_in.dropna()])).value_counts()
 
 
 @app.route('/')
 @cross_origin()
 def index():
-    return df.to_json(orient='records', force_ascii=False)
+    if 'order' in request.form and request.form['order'] == 'desc' and 'take' in request.form:
+        return df.sort_values(by="release_year").title[:request.form['take']].to_json(orient='records', force_ascii=False)
+    else:
+        return df.to_json(orient='records', force_ascii=False)
+
+
+@app.route('/movie')
+@cross_origin()
+def movie():
+    if 'order' in request.form and request.form['order'] == 'desc' and 'take' in request.form:
+        return df[df['type'] == 'Movie'].sort_values(by="release_year").title[:request.form['take']].to_json(orient='records', force_ascii=False)
+    else:
+        return df.to_json(orient='records', force_ascii=False)
+
+
+@app.route('/tvshow')
+@cross_origin()
+def tv_show():
+    if 'order' in request.form and request.form['order'] == 'desc' and 'take' in request.form:
+        return df[df['type'] == 'Movie'].sort_values(by="release_year").title[:request.form['take']].to_json(orient='records', force_ascii=False)
+    else:
+        return df.to_json(orient='records', force_ascii=False)
 
 
 @app.route('/type')
@@ -53,15 +77,24 @@ def country_top5():
 
 @app.route('/releaseyear/top10')
 @cross_origin()
-def release_year_top5():
+def release_year_top10():
     return df['release_year'].value_counts()[:10].to_json(orient='columns', force_ascii=False)
 
 
 @app.route('/releaseyear/bottom10')
 @cross_origin()
-def release_year_bottom5():
+def release_year_bottom10():
     return df['release_year'].value_counts()[-10:].to_json(orient='columns', force_ascii=False)
 
 
+@app.route('/listedin/top10')
+@cross_origin()
+def listed_in_top10():
+    return listed_in_counts[:10].to_json(orient='columns', force_ascii=False)
+
+
 if __name__ == '__main__':
-    app.run(port=80)
+    app.config['ENV'] = 'development'
+    app.config['DEBUG'] = True
+    app.config['TESTING'] = True
+    app.run(port=80, debug=True)
